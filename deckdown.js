6dda925196d6b1bcbebb95418d017987236ee6a9
Alan Languirand <alan@13protons.com>
2015-05-06 21:59:45 -0400

    reverting server changes

--- deckdown.js
+++ deckdown.js
@@ -12,11 +12,12 @@ var fs = require("fs")
   , app = express();
 
 kramed.setOptions({
-  'breaks': true
+
 });
 
 //load the templates
 var template = {
+  main: fs.readFileSync(process.cwd() + "/templates/index.html", "utf8"),
   slides: fs.readFileSync(process.cwd() + "/templates/masters/default.html", "utf8")
 };
 var timer = Date.now();
@@ -25,7 +26,7 @@ app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/templates');
 app.set('view engine', 'html');
 
-//less is more? 
+//less is more?
 app.use(lessMiddleware(path.join(__dirname, '/public')));
 
 app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
@@ -33,8 +34,8 @@ app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of cur
 app.use('/deck', function(req, res, next){
   timer = Date.now();
   res.deck = {
-        title: 'no data', 
-        template: 'default', 
+        title: 'no data',
+        template: 'default',
         content:"<section><h1>Deck not found</h1><p>Please check that url, and try again.</p></section>"
       }
   next();
@@ -47,7 +48,7 @@ app.get('/', function (req, res) {
   var page = kramed(fs.readFileSync(process.cwd() + "/README.md", "utf8"));
   //little hackey - removes anything before the first <a>
   page = '<p><a>' + page.substr(page.indexOf('</a>')) ;
-              
+
   res.render('index', {content: page});
 });
 
@@ -79,11 +80,11 @@ function getDeckCtrl(req, res, next){
         var _names = url.match(/[A-Z,a-z]+/g);
 
         res.deck = extend(res.deck, {
-          title: _names[_names.length - 2], 
+          title: _names[_names.length - 2],
           content: slides(body)
         });
       }
-      
+
       gen();
       res.render('deck', res.deck);
     });
@@ -100,14 +101,14 @@ function postDeckCtrl (req, res, next){
   //console.log(req.param('markdown'));
   var md = req.param('markdown');
   res.deck.template = req.param('theme');
-  
+
   if(validator.isURL(md, {allow_underscores: true })){
     //save url somewhere
     res.deckUrl = md;
     next();
   }else{
     var s = slides(md);
-    gen();  
+    gen();
     res.deck = extend(res.deck, {
       title: 'Created With Deckdown',
       content: s
@@ -118,9 +119,9 @@ function postDeckCtrl (req, res, next){
 
 function slides(body){
   var rawSlides = mdToHtmlArray(body);
-  
+
   console.log(rawSlides);
-  
+
   //combine
   var slides = rawSlides.map(function(slide, i){
     return ejs.render(template.slides, {
@@ -128,8 +129,8 @@ function slides(body){
       content: slide.content
     });
   });
-  
-  
+
+
   return slides.join("");
 }
 
@@ -150,9 +151,9 @@ match the whole +()
 */
     //var properties = markdown.match($2)
     //var matches, properties = [];
-   
+
     properties = markdown.match(/\+\(.*\)/g);
-  
+
     if(properties){
       properties.forEach(function(p, i){
         //remove +()
@@ -163,15 +164,15 @@ match the whole +()
     //console.log(properties);
     var html = kramed(markdown);
     //Array of values we need to prepend after the split
-    var headers = html.match(/(<h[1-6])|(<hr>)/g); 
-    
+    var headers = html.match(/(<h[1-6])|(<hr>)/g);
+
     //leave a marker for splitting
-    html = html.replace(/(<h[1-6])|(<hr>)/g, '<========/*slide*/=========>'); 
-    var slides = html.split("<========/*slide*/=========>"); 
-    
+    html = html.replace(/(<h[1-6])|(<hr>)/g, '<========/*slide*/=========>');
+    var slides = html.split("<========/*slide*/=========>");
+
     //element 0 is whitespace. artifact of split method
-    slides.shift(); 
-    
+    slides.shift();
+
     slides = slides.map(function(s, i){
       //add proper header number back after it was lost in the replace
       var result = {};
@@ -186,15 +187,9 @@ match the whole +()
           'level': headers[i].slice(1),
           'content': headers[i] + s
         };
-      }      
-      return result; 
+      }
+      return result;
     });
-    
+
     return slides;
   }
-
-  
-
-
-
-
